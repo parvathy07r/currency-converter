0fd2a5da80eff91d8066d40cf513de2d13856156
Parvathy R <parvathy07r@gmail.com>
2024-07-10 18:50:11 +0530

    Modified Task three
    - Added Form
    - Added Error Message

--- taskthree.js
+++ taskthree.js
@@ -1,28 +1,81 @@
-const amount = document.querySelector(".given-amount");
-const fromValue = document.querySelector(".fromValue");
-const toValue = document.querySelector(".toValue");
-const button = document.querySelector("button");
+const currencyForm =  document.querySelector("#currency_form");
+const result = document.querySelector(".result");
 
-button.addEventListener("click", function(){
-    const searchQuery = amount.value;
-    const fromCurrency = getCurrencyCode(fromValue.value);
-    const toCurrency = getCurrencyCode(toValue.value);
-    const result = document.querySelector(".result");
+currencyForm.addEventListener("submit", function(event){
+    event.preventDefault();
 
-    fetch(`https://api.frankfurter.app/latest?amount=${searchQuery}&from=${fromCurrency}&to=${toCurrency}`)
+    const form = event.target;
+
+    const amount = form["givenAmount"].value;
+    const fromValue = getCurrencyCode(form["fromValue"].value);
+    const toValue = getCurrencyCode(form["toValue"].value);
+
+
+
+    if(!Number.isInteger(Number(amount))){
+        result.innerHTML = ""
+        const node = document.createElement("p");
+        const textnode = document.createTextNode("Please enter a valid integer value !!!");
+        node.append(textnode);
+        result.append(node);
+        node.style.color = "red";
+        return;
+    }
+
+    if (!fromValue || !toValue) {
+        result.innerHTML = ""
+        const node = document.createElement("p");
+        const textnode = document.createTextNode("Please select valid currencies.");
+        node.appendChild(textnode);
+        node.style.color = "red";
+        result.appendChild(node);
+        return;
+    }
+
+    fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromValue}&to=${toValue}`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
-        console.log(data.rates[toCurrency]);
         result.innerHTML = '';
         const node = document.createElement("p");
-        const textnode = document.createTextNode(`${data.rates[toCurrency]} ${Object.keys(data.rates)}`);
+        const textnode = document.createTextNode(`${data.rates[toValue]} ${Object.keys(data.rates)}`);
         node.append(textnode);
-        result.append(node);
-        
+        result.append(node);       
     })
-});
+    .catch(function(error){
+        console.error(error);
+    })
+
+
+
+
+})
+// const amount = document.querySelector(".given-amount");
+// const fromValue = document.querySelector(".fromValue");
+// const toValue = document.querySelector(".toValue");
+// const button = document.querySelector("button");
+
+// button.addEventListener("click", function(){
+//     const searchQuery = amount.value;
+//     const fromCurrency = getCurrencyCode(fromValue.value);
+//     const toCurrency = getCurrencyCode(toValue.value);
+//     const result = document.querySelector(".result");
+
+//     fetch(`https://api.frankfurter.app/latest?amount=${searchQuery}&from=${fromCurrency}&to=${toCurrency}`)
+//     .then(function(response){
+//         return response.json();
+//     })
+//     .then(function(data){
+//         console.log(data.rates[toCurrency]);
+//         result.innerHTML = '';
+//         const node = document.createElement("p");
+//         const textnode = document.createTextNode(`${data.rates[toCurrency]} ${Object.keys(data.rates)}`);
+//         node.append(textnode);
+//         result.append(node);
+        
+//     })
+// });
 
 function getCurrencyCode(code) {
     if(code === "1"){
