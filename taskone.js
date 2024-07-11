c297659ee2e5c2b059e0936659bc8ee83069ac76
Parvathy R <parvathy07r@gmail.com>
2024-07-11 12:14:16 +0530

    Added input validation
    - Added a check for non-numeric input to display "Please enter a valid number!".
    - Added a check for values less than or equal to zero to display "Please enter a value greater than 0!".

--- taskone.js
+++ taskone.js
@@ -1,35 +1,46 @@
-// const input = document.querySelector("input");
 const button = document.querySelector("#submit_button");
-// const result =  document.querySelector(".result");
 const currencyForm = document.querySelector("#currency_form");
 const toValue = document.querySelector(".toValue");
 
 currencyForm.addEventListener("submit", function(event){
+
     event.preventDefault();
+
     toValue.innerHTML = "";
-    // const searchQuery = input.value;
+
     const form = event.target;
-    const fromValue = form["fromValue"].value;
-    if(!Number.isInteger(Number(fromValue))){
-        const node = document.createElement("p");
-        const textnode = document.createTextNode("Please enter a valid integer value !!!");
-        node.append(textnode);
-        toValue.append(node);
-        node.style.color = "red";
+    const fromValue = Number.parseFloat(form["fromValue"].value);
+
+    if(isNaN(fromValue)){
+        toValue.append(getErrorMessage("Please enter a number !!!!"));
         return;
     }
+
+    if(fromValue <= 0){
+        toValue.append(getErrorMessage("Please enter a number greater than 0 !!!!"));
+        return;
+    }
+
+    function getErrorMessage(message){
+        const node = document.createElement("p");
+        node.innerHTML = message;
+        node.style.color = "red";
+        return node;
+    }
+
     fetch(`https://api.frankfurter.app/latest?amount=${fromValue}&from=INR&to=JPY`)
     .then(function(response){
         return response.json();
     })
     .then(function(data){
         console.log(data.rates.JPY);
+
         const node =  document.createElement("p");
-        const textnode = `${data.rates.JPY} JPY`;
-        node.append(textnode);
+        node.innerHTML = `${data.rates.JPY} JPY`;
         toValue.append(node);
     })
     .catch(function(error){
-        console.log(error);
+        console.error(error);
     })
+
 });
