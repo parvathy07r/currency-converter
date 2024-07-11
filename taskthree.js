1ea13be7d3ee41e9deb92113715752141c77b686
Parvathy R <parvathy07r@gmail.com>
2024-07-11 15:17:37 +0530

    Added input validation
    - Added validation to check if the amount is a number
    - Added validation to check if the currency code is valid

--- taskthree.js
+++ taskthree.js
@@ -1,92 +1,69 @@
-const currencyForm =  document.querySelector("#currency_form");
+const currencyForm = document.querySelector("#currency_form");
 const result = document.querySelector(".result");
 
-currencyForm.addEventListener("submit", function(event){
+currencyForm.addEventListener("submit", function (event) {
+
     event.preventDefault();
 
     const form = event.target;
-
-    const amount = form["givenAmount"].value;
+    const amount = Number.parseFloat(form["givenAmount"].value);
     const fromValue = getCurrencyCode(form["fromValue"].value);
     const toValue = getCurrencyCode(form["toValue"].value);
 
+    result.innerHTML = "";
 
+    // Validating fromValue
+    if (isNaN(amount)) {
+        result.append(getErrorMessage("Please enter a number !!!"));
+        return;
+    }
 
-    if(!Number.isInteger(Number(amount))){
-        result.innerHTML = ""
-        const node = document.createElement("p");
-        const textnode = document.createTextNode("Please enter a valid integer value !!!");
-        node.append(textnode);
-        result.append(node);
-        node.style.color = "red";
+    if (amount <= 0) {
+        result.append(getErrorMessage("Please enter a number greater than 0 !!!"));
         return;
     }
 
     if (!fromValue || !toValue) {
-        result.innerHTML = ""
-        const node = document.createElement("p");
-        const textnode = document.createTextNode("Please select valid currencies.");
-        node.appendChild(textnode);
-        node.style.color = "red";
-        result.appendChild(node);
+        result.appendChild(getErrorMessage("Please enter valid currency code !!!!"));
         return;
     }
 
-    fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromValue}&to=${toValue}`)
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(data){
-        result.innerHTML = '';
+    //function for displaying error message
+    function getErrorMessage(message) {
         const node = document.createElement("p");
-        const textnode = document.createTextNode(`${data.rates[toValue]} ${Object.keys(data.rates)}`);
-        node.append(textnode);
-        result.append(node);       
-    })
-    .catch(function(error){
-        console.error(error);
-    })
+        node.innerHTML = message;
+        node.style.color = "red";
+        return node;
+    }
 
-
-
+    fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${fromValue}&to=${toValue}`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            result.innerHTML = '';
+            const node = document.createElement("p");
+            node.innerHTML = `${data.rates[toValue]} ${Object.keys(data.rates)}`
+            result.append(node);
+        })
+        .catch(function (error) {
+            console.error(error);
+        })
 
 })
-// const amount = document.querySelector(".given-amount");
-// const fromValue = document.querySelector(".fromValue");
-// const toValue = document.querySelector(".toValue");
-// const button = document.querySelector("button");
 
-// button.addEventListener("click", function(){
-//     const searchQuery = amount.value;
-//     const fromCurrency = getCurrencyCode(fromValue.value);
-//     const toCurrency = getCurrencyCode(toValue.value);
-//     const result = document.querySelector(".result");
-
-//     fetch(`https://api.frankfurter.app/latest?amount=${searchQuery}&from=${fromCurrency}&to=${toCurrency}`)
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(data){
-//         console.log(data.rates[toCurrency]);
-//         result.innerHTML = '';
-//         const node = document.createElement("p");
-//         const textnode = document.createTextNode(`${data.rates[toCurrency]} ${Object.keys(data.rates)}`);
-//         node.append(textnode);
-//         result.append(node);
-        
-//     })
-// });
-
+// function to get currency codes
 function getCurrencyCode(code) {
-    if(code === "1"){
-        return "JPY"
-    }else if(code === "2") {
-        return "INR"
-    }else if(code === "3"){
-        return "USD"
-    }else if(code === "4"){
-        return "CHF"
-    }else{
-        return null;
+    switch(parseInt(code)) {
+        case 1:
+            return "JPY";
+        case 2:
+            return "INR";
+        case 3:
+            return "USD";
+        case 4:
+            return "CHF"
+        default:
+            return null;
     }
 }
